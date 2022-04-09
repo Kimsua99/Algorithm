fa0eae70da00c53d88bad01e3a2c3f67db9f7ccb
수아 <79245006+Kimsua99@users.noreply.github.com>
2022-04-10 04:05:13 +0900

    Add files via upload

---
+++ Inflearn_Algorithm/1. 1부터 N까지의 M의 배수합.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	int sum = 0;
+	
+	cin >> n >> m;
+	
+	for(int i=1; i<=n; i++)
+	{
+		if(i % m == 0)//m이 i의 배수이면 
+		{
+			sum += i; 
+		}
+	}
+	
+	cout << sum;
+	return 0;
+}
+
