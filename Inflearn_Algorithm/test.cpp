f30f765e15ee0b52200f528554bd1477f57fbce1
수아 <79245006+Kimsua99@users.noreply.github.com>
2022-04-11 04:06:26 +0900

    Add files via upload

---
+++ Inflearn_Algorithm/test.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	int sum = 1;
+
+	cin >> n;
+
+	cout << "1";
+
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			cout << "+" << i;
+			sum += i;
+		}
+	}
+	cout << "=" << sum;
+	return 0;
+}
